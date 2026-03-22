492d70fb698a2bc7de6d1cca9e56c6adc701d712
Khalnn <1620339196@qq.com>
2026-03-22 10:34:07 +0800

    Initial import (history squashed)

---
+++ config/object_varant.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+ROOT_DIR = Path(__file__).parent.parent
+asset_dir = ROOT_DIR / "asset"
+table_config = {
+    "urdf_path": str(asset_dir / "scene/table/table.urdf"),
+    "object_name": "table",
+    "position": [0, 0, 0],
+    "orientation": [1, 0, 0, 0]
+}
+
+banana_config = {
+    "object_collision_meshes": str(asset_dir / "object/banana/collision.obj"),
+    "object_visual_meshes": str(asset_dir / "object/banana/visual.glb"),
+    "object_name": "banana"
+}
+cup_config = {
+    "object_collision_meshes": str(asset_dir / "object/cup/textured.obj"),
+    "object_visual_meshes": str(asset_dir / "object/cup/base.glb"),
+    "object_name": "cup",
+    "position": [0.5, 0, 0],
+    "orientation": [1, 0, 0, 0]
+}
+
+bottle_config = {
+    "urdf_path": str(asset_dir / "object/3517/mobility.urdf"),
+    "object_name": "bottle",
+    "position": [0.5, 0, 0],
+    "orientation": [1, 0, 0, 0]
+}
+drawer_config = {
+    "urdf_path": str(asset_dir / "object/19179/mobility.urdf"),
+    "object_name": "drawer",
+    "position": [1.5, 0, 0],
+    "orientation": [1, 0, 0, 0]
+}
---
+++ robot/camera.py
@@ -0,0 +1,8 @@
+from .controller import p
+
+class Camera:
+    def __init__(self,camera_config):
+        pass
+    
+    def get_camera_image(self):
+        pass
